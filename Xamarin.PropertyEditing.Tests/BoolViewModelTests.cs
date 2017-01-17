ce09456fdd7d6652ed45d7a7d7932348f22c0bc2
Eric Maupin <ermaup@microsoft.com>
2017-01-17 15:26:51 -0500

    Add bool property tests

---
+++ Xamarin.PropertyEditing.Tests/BoolViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.PropertyEditing.ViewModels;
+
+namespace Xamarin.PropertyEditing.Tests
+{
+	class BoolViewModelTests
+		: PropertyViewModelTests<bool>
+	{
+		protected override bool GetRandomTestValue (Random rand)
+		{
+			return (rand.Next (0, 1) == 1);
+		}
+
+		protected override PropertyViewModel<bool> GetViewModel (IPropertyInfo property, IEnumerable<IObjectEditor> editors)
+		{
+			return new PropertyViewModel<bool> (property, editors);
+		}
+	}
+}
