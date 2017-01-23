3c37287478b343788c4701862b6bd0ab314b38eb
Eric Maupin <ermaup@microsoft.com>
2017-01-23 17:06:44 -0500

    ValueInfo equatable

---
+++ Xamarin.PropertyEditing.Tests/ValueInfoTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+
+namespace Xamarin.PropertyEditing.Tests
+{
+	[TestFixture]
+	internal class ValueInfoTests
+	{
+		[Test]
+		public void ValueEquatable ()
+		{
+			var left = new ValueInfo<int> { Value = 5 };
+			var right = new ValueInfo<int> { Value = 5 };
+
+			AssertEqual (left, right);
+		}
+
+		[Test]
+		public void SourceEquatable ()
+		{
+			var left = new ValueInfo<int> { Source = ValueSource.Binding };
+			var right = new ValueInfo<int> { Source = ValueSource.Binding };
+
+			AssertEqual (left, right);
+		}
+
+		[Test]
+		public void DescriptorEquatable ()
+		{
+			string description = "monkeys";
+			var left = new ValueInfo<int> { ValueDescriptor = description };
+			var right = new ValueInfo<int> { ValueDescriptor = description };
+
+			AssertEqual (left, right);
+		}
+
+		[Test]
+		public void ValueButNotSourceUnequal ()
+		{
+			var left = new ValueInfo<int> {
+				Value = 5,
+				Source = ValueSource.Local
+			};
+
+			var right = new ValueInfo<int> {
+				Value = 5,
+				Source = ValueSource.Binding
+			};
+
+			AssertNotEqual (left, right);
+		}
+
+		private void AssertNotEqual<T> (ValueInfo<T> left, ValueInfo<T> right)
+		{
+			Assert.That (left, Is.Not.EqualTo (right));
+			Assert.That (left != right);
+
+			if (left != null)
+				Assert.That (left.GetHashCode (), Is.Not.EqualTo (right.GetHashCode ()));
+		}
+
+		private void AssertEqual<T> (ValueInfo<T> left, ValueInfo<T> right)
+		{
+			Assert.That (left, Is.EqualTo (right));
+			Assert.That (left == right);
+
+			if (left != null)
+				Assert.That (left.GetHashCode (), Is.EqualTo (right.GetHashCode ()));
+		}
+	}
+}
--- Xamarin.PropertyEditing/ValueInfo.cs
+++ Xamarin.PropertyEditing/ValueInfo.cs
@@ -1,6 +1,13 @@
-﻿namespace Xamarin.PropertyEditing
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xamarin.PropertyEditing
 {
+	/// <remarks>
+	/// <para>The <see cref="Source"/> and <see cref="ValueDescriptor"/> for the value must match for two ValueInfos to be considered equal.</para>
+	/// </remarks>
 	public class ValueInfo<T>
+		: IEquatable<ValueInfo<T>>
 	{
 		public T Value
 		{
@@ -21,6 +28,47 @@
 		{
 			get;
 			set;
+		}
+
+		public bool Equals (ValueInfo<T> other)
+		{
+			if (ReferenceEquals (null, other))
+				return false;
+			if (ReferenceEquals (this, other))
+				return true;
+
+			return EqualityComparer<T>.Default.Equals (Value, other.Value) && Equals (ValueDescriptor, other.ValueDescriptor) && Source == other.Source;
+		}
+
+		public override bool Equals (object obj)
+		{
+			if (ReferenceEquals (null, obj))
+				return false;
+			if (ReferenceEquals (this, obj))
+				return true;
+			if (obj.GetType () != GetType ())
+				return false;
+			return Equals ((ValueInfo<T>) obj);
+		}
+
+		public override int GetHashCode ()
+		{
+			unchecked {
+				var hashCode = EqualityComparer<T>.Default.GetHashCode (Value);
+				hashCode = (hashCode * 397) ^ (ValueDescriptor?.GetHashCode () ?? 0);
+				hashCode = (hashCode * 397) ^ (int) Source;
+				return hashCode;
+			}
+		}
+
+		public static bool operator == (ValueInfo<T> left, ValueInfo<T> right)
+		{
+			return Equals (left, right);
+		}
+
+		public static bool operator != (ValueInfo<T> left, ValueInfo<T> right)
+		{
+			return !Equals (left, right);
 		}
 	}
 }
