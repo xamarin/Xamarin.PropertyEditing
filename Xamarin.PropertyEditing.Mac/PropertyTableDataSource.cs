ebca53deaa6afba2e02a17e3886cb296014e34d5
Madalyn Coryea <madalyn.coryea@xamarin.com>
2017-01-25 16:30:46 -0500

    Putting table setup classes in their own files

---
+++ Xamarin.PropertyEditing.Mac/PropertyTableDataSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using AppKit;
+using Xamarin.PropertyEditing.ViewModels;
+
+namespace Xamarin.PropertyEditing.Mac
+{
+	public class PropertyTableDataSource : NSTableViewDataSource
+	{
+		internal PropertyTableDataSource (PanelViewModel viewModel /*IEditorProvider data*/)
+		{
+			ViewModel = viewModel; //new PanelViewModel (data);
+		}
+
+		internal PanelViewModel ViewModel { get; private set; }
+		public ICollection<object> SelectedItems => this.ViewModel.SelectedObjects;
+
+		public override nint GetRowCount (NSTableView tableView)
+		{
+			return ViewModel.Properties.Count;
+		}
+	}
+}
---
+++ Xamarin.PropertyEditing.Mac/PropertyTableDelegate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using AppKit;
+using Xamarin.PropertyEditing.ViewModels;
+
+namespace Xamarin.PropertyEditing.Mac
+{
+	public class PropertyTableDelegate : NSTableViewDelegate
+	{
+		const string CellIdentifier = "Cell";
+		PropertyTableDataSource DataSource;
+
+		public PropertyTableDelegate (PropertyTableDataSource datasource)
+		{
+			this.DataSource = datasource;
+		}
+
+		// the table is looking for this method, picks it up automagically
+		public override NSView GetViewForItem (NSTableView tableView, NSTableColumn tableColumn, nint row)
+		{
+			var property = DataSource.ViewModel.Properties [(int)row];
+
+			// This pattern allows you reuse existing views when they are no-longer in use.
+			// If the returned view is null, you instance up a new view
+			// If a non-null view is returned, you modify it enough to reflect the new data
+
+			// TODO: need to check CellIdentifier here?
+			NSView view = (NSView)tableView.MakeView (CellIdentifier, this);
+			if (view == null) {
+				if (property is StringPropertyViewModel) {
+					view = new StringEditorControl ();
+					view.Identifier = CellIdentifier;
+				}
+			}
+
+			// TODO: can do this differently
+			// Setup view based on the column selected
+			switch (tableColumn.Title) {
+			case "Properties":
+				//view.StringValue = DataSource.Properties [(int)row].Name;
+				if (view is StringEditorControl) {
+					((StringEditorControl)view).StringEditor.StringValue = property.Property.Name;
+				}
+				break;
+			case "Editors":
+				//view.StringValue = DataSource.Properties [(int)row].PropertyValue;
+				if (view is StringEditorControl) {
+					((StringEditorControl)view).StringEditor.StringValue = property.Property.Type.ToString ();
+				}
+				break;
+			}
+
+			return view;
+		}
+	}
+}
