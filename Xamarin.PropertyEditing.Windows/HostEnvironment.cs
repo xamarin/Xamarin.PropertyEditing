b94d5a334659f3b78b5867a8dc55a0684dfc9905
Eric Maupin <ermaup@microsoft.com>
2017-05-31 18:27:50 -0400

    [Win] Begin styling context menu

---
+++ Xamarin.PropertyEditing.Windows/HostEnvironment.cs
@@ -0,0 +1,40 @@
+﻿namespace Xamarin.PropertyEditing.Windows
+{
+	internal class HostEnvironment
+		: NotifyingObject
+	{
+		public static HostEnvironment Current
+		{
+			get;
+		} = new HostEnvironment();
+
+		public bool AreAnimationsAllowed
+		{
+			get { return this.areAnimationsAllowed; }
+			set
+			{
+				if (this.areAnimationsAllowed == value)
+					return;
+
+				this.areAnimationsAllowed = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public bool AreGradientsAllowed
+		{
+			get { return this.areGradientsAllowed; }
+			set
+			{
+				if (this.areGradientsAllowed == value)
+					return;
+
+				this.areGradientsAllowed = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private bool areAnimationsAllowed = true;
+		private bool areGradientsAllowed = true;
+	}
+}
