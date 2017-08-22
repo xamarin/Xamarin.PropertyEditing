ead0677ef8da9af2f6184bf1f53e9f5a8dcd96d5
Eric Maupin <ermaup@microsoft.com>
2017-08-22 15:39:36 -0400

    [Core] Make PropertyVariation equatable

--- Xamarin.PropertyEditing/PropertyVariation.cs
+++ Xamarin.PropertyEditing/PropertyVariation.cs
@@ -3,6 +3,7 @@
 namespace Xamarin.PropertyEditing
 {
 	public class PropertyVariation
+		: IEquatable<PropertyVariation>
 	{
 		public PropertyVariation (string category, string name)
 		{
@@ -23,6 +24,37 @@ namespace Xamarin.PropertyEditing
 		public string Name
 		{
 			get;
+		}
+
+		public virtual bool Equals (PropertyVariation other)
+		{
+			if (ReferenceEquals (null, other))return false;
+			if (ReferenceEquals (this, other)) return true;
+			return String.Equals (Category, other.Category) && String.Equals (Name, other.Name);
+		}
+
+		public override bool Equals (object obj)
+		{
+			if (ReferenceEquals (null, obj)) return false;
+			if (ReferenceEquals (this, obj)) return true;
+			return Equals (obj as PropertyVariation);
+		}
+
+		public override int GetHashCode ()
+		{
+			unchecked {
+				return ((Category != null ? Category.GetHashCode () : 0) * 397) ^ (Name != null ? Name.GetHashCode () : 0);
+			}
+		}
+
+		public static bool operator == (PropertyVariation left, PropertyVariation right)
+		{
+			return Equals (left, right);
+		}
+
+		public static bool operator != (PropertyVariation left, PropertyVariation right)
+		{
+			return !Equals (left, right);
 		}
 	}
 }
