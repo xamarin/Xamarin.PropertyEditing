56bec238ef80908e2d35ffab9114e118e78f2ec6
Eric Maupin <ermaup@microsoft.com>
2017-09-11 14:50:24 -0400

    [Win] Object naming / type display

---
+++ Xamarin.PropertyEditing.Windows/OppositeBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Xamarin.PropertyEditing.Windows
+{
+	internal class OppositeBoolConverter
+		: IValueConverter
+	{
+		public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null || !(value is bool))
+				return null;
+
+			return !(bool)value;
+		}
+
+		public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
---
+++ Xamarin.PropertyEditing.Windows/SubmitTextBox.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Xamarin.PropertyEditing.Windows
+{
+	/// <remarks>
+	/// This textbox will update its Text binding also when enter is pressed
+	/// </remarks>
+    internal class SubmitTextBox
+		: TextBox
+    {
+		public SubmitTextBox()
+		{
+			PreviewKeyDown += OnKeyDown;
+		}
+
+		public static readonly DependencyProperty FocusSelectsAllProperty = DependencyProperty.Register ("FocusSelectsAll", typeof (bool), typeof (SubmitTextBox));
+
+		public bool FocusSelectsAll
+		{
+			get { return (bool)GetValue (FocusSelectsAllProperty); }
+			set { SetValue (FocusSelectsAllProperty, value); }
+		}
+
+		protected override void OnPreviewMouseDown (MouseButtonEventArgs e)
+		{
+			if (!IsKeyboardFocusWithin) {
+				e.Handled = true;
+				Focus ();
+			}
+
+			base.OnPreviewMouseDown (e);
+		}
+
+		protected override void OnGotKeyboardFocus (KeyboardFocusChangedEventArgs e)
+		{
+			base.OnGotKeyboardFocus (e);
+
+			if (FocusSelectsAll)
+				SelectAll ();
+		}
+
+		private void OnKeyDown (object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if (e.Key != Key.Enter)
+				return;
+
+			var expression = GetBindingExpression (TextProperty);
+			expression?.UpdateSource ();
+		}
+	}
+}
