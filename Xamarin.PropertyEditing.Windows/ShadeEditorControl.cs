a9c74ada36fadf0a94e77ad4c80ef823027c50a6
Bertrand Le Roy <beleroy@microsoft.com>
2017-10-16 17:37:39 -0700

    Refactor shade picker into its own control: initial work

---
+++ Xamarin.PropertyEditing.Windows/ShadeEditorControl.cs
@@ -0,0 +1,150 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Xamarin.PropertyEditing.Drawing;
+
+namespace Xamarin.PropertyEditing.Windows
+{
+	internal class ShadeEditorControl : Control
+	{
+		public ShadeEditorControl()
+		{
+			DefaultStyleKey = typeof (ShadeEditorControl);
+		}
+
+		Rectangle saturationLayer;
+		Rectangle luminosityLayer;
+		Canvas cursor;
+
+		public static readonly DependencyProperty ShadeProperty =
+			DependencyProperty.Register (
+				"Shade", typeof (CommonColor), typeof (ShadeEditorControl),
+				new PropertyMetadata (new CommonColor (0, 0, 0), OnShadeChanged));
+
+		public CommonColor Shade {
+			get => (CommonColor)GetValue (ShadeProperty);
+			set => SetValue (ShadeProperty, value);
+		}
+
+		public static readonly DependencyProperty CursorPositionProperty =
+			DependencyProperty.Register (
+				"CursorPosition", typeof (Point), typeof (ShadeEditorControl),
+				new PropertyMetadata (new Point (0, 0)));
+
+		Point CursorPosition {
+			get => (Point)GetValue (CursorPositionProperty);
+			set => SetValue (CursorPositionProperty, value);
+		}
+
+		public static readonly DependencyProperty HueProperty =
+			DependencyProperty.Register (
+				"Hue", typeof (CommonColor), typeof (ShadeEditorControl),
+				new PropertyMetadata (new CommonColor (255, 0, 0), HueChanged));
+
+		public CommonColor Hue {
+			get => (CommonColor)GetValue (HueProperty);
+			set => SetValue (HueProperty, value);
+		}
+
+		public override void OnApplyTemplate ()
+		{
+			base.OnApplyTemplate ();
+
+			saturationLayer = (Rectangle)GetTemplateChild ("saturationLayer");
+			luminosityLayer = (Rectangle)GetTemplateChild ("luminosityLayer");
+			cursor = (Canvas)GetTemplateChild ("cursor");
+
+			luminosityLayer.MouseLeftButtonDown += OnCursorMoved;
+			luminosityLayer.MouseMove += (s, e) => {
+				if (e.LeftButton == MouseButtonState.Pressed) {
+					OnCursorMoved (s, e);
+				}
+			};
+		}
+
+		void OnCursorMoved(object s, MouseEventArgs e)
+		{
+			CursorPosition = e.GetPosition ((IInputElement)s);
+			SetValue (ShadeProperty, GetColorFromPosition (CursorPosition));
+		}
+
+		static void OnShadeChanged (DependencyObject source, DependencyPropertyChangedEventArgs e)
+		{
+			var that = source as ShadeEditorControl;
+			if (that == null || that.cursor == null) return;
+			that.CursorPosition = that.GetPositionFromColor ((CommonColor)e.NewValue);
+		}
+
+		static void HueChanged (DependencyObject source, DependencyPropertyChangedEventArgs e)
+		{
+			var that = source as ShadeEditorControl;
+			if (that == null || that.saturationLayer == null) return;
+			var saturationLayer = that.saturationLayer;
+			var newBrush = (LinearGradientBrush)saturationLayer.Fill.Clone ();
+			var gradientStops = newBrush.GradientStops;
+			gradientStops.RemoveAt (1);
+			var currentHue = that.Hue;
+			gradientStops.Add (new GradientStop (Color.FromRgb (currentHue.R, currentHue.G, currentHue.B), 1));
+			saturationLayer.Fill = newBrush;
+			that.SetValue(ShadeProperty, that.GetColorFromPosition (that.CursorPosition));
+		}
+
+		/// <summary>
+		/// Maps coordinates within the shade chooser gradients into colors.
+		/// The gradients have the current hue on the top-right corner,
+		/// black along the whole bottom border, and white of the top-left.
+		///
+		/// For example, with a hue of 128,255,0:
+		/// 
+		/// 255,255,255---192,255,128---128,255,000
+		///      |             |             |
+		/// 128,128,128---096,128,064---064,128,000
+		///      |             |             |
+		/// 000,000,000---000,000,000---000,000,000
+		/// 
+		/// The horizontal axis corresponds roughly to saturation, and the
+		/// vertical axis to brightness.
+		/// </summary>
+		/// <param name="position">The position for which to infer the color</param>
+		/// <returns>The shade</returns>
+		CommonColor GetColorFromPosition (Point position)
+		{
+			var saturation = position.X / saturationLayer.ActualWidth;
+			var brightness = 1 - position.Y / luminosityLayer.ActualHeight;
+
+			return new CommonColor (
+				(byte)((255 + (Hue.R - 255) * saturation) * brightness),
+				(byte)((255 + (Hue.G - 255) * saturation) * brightness),
+				(byte)((255 + (Hue.B - 255) * saturation) * brightness),
+				255
+			);
+		}
+
+		/// <summary>
+		/// Finds a position on the shade chooser that corresponds to the passed-in
+		/// color.
+		/// </summary>
+		/// <param name="color">The color for which we want the coordinates</param>
+		/// <returns>The coordinates of the shade in the shade chooser</returns>
+		Point GetPositionFromColor (CommonColor color)
+		{
+			var hue = color.ToHue ();
+			var isRedMaxed = hue.R == 255;
+			var isGreenMaxed = hue.G == 255;
+			var alphaFactor = isRedMaxed ? (double)color.R / 255 : isGreenMaxed ? (double)color.G / 255 : (double)color.B / 255;
+			var isRedMin = hue.R == 0;
+			var isGreenMin = hue.G == 0;
+
+			var xrate =
+				isRedMin ? (color.R / alphaFactor - 255) / (hue.R - 255)
+				: isGreenMin ? (color.G / alphaFactor - 255) / (hue.G - 255)
+				: (color.B / alphaFactor - 255) / (hue.B - 255);
+
+			return new Point (
+				xrate * saturationLayer.ActualWidth + saturationLayer.Margin.Left ,
+				(1 - alphaFactor) * luminosityLayer.ActualHeight + luminosityLayer.Margin.Top);
+		}
+	}
+}
--- Xamarin.PropertyEditing.Windows/SolidBrushEditorControl.cs
+++ Xamarin.PropertyEditing.Windows/SolidBrushEditorControl.cs
@@ -1,11 +1,7 @@
-using System.ComponentModel;
-using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Shapes;
 using Xamarin.PropertyEditing.Drawing;
 using Xamarin.PropertyEditing.ViewModels;
 
@@ -24,16 +20,10 @@ namespace Xamarin.PropertyEditing.Windows
 		TextBox greenEntry;
 		TextBox blueEntry;
 		TextBox alphaEntry;
-		Rectangle topShadeChooser;
-		Rectangle bottomShadeChooser;
 		HueEditorControl hueChooser;
-		Canvas cursor;
-		double cursorWidth;
-		double cursorHeight;
+		ShadeEditorControl shadeChooser;
 
 		SolidBrushPropertyViewModel ViewModel => DataContext as SolidBrushPropertyViewModel;
-
-		Point CurrentCursorPosition;
 
 		public override void OnApplyTemplate ()
 		{
@@ -49,16 +39,9 @@ namespace Xamarin.PropertyEditing.Windows
 			blueEntry = (TextBox)GetTemplateChild ("blueEntry");
 			alphaEntry = (TextBox)GetTemplateChild ("alphaEntry");
 			hueChooser = (HueEditorControl)GetTemplateChild ("hueChooser");
-			topShadeChooser = (Rectangle)GetTemplateChild ("topShadeChooser");
-			bottomShadeChooser = (Rectangle)GetTemplateChild ("bottomShadeChooser");
-			cursor = (Canvas)GetTemplateChild ("cursor");
-			cursorWidth = (cursor.Children.Cast<Shape> ()).Max (c => c.Width);
-			cursorHeight = (cursor.Children.Cast<Shape> ()).Max (c => c.Height);
+			shadeChooser = (ShadeEditorControl)GetTemplateChild ("shadeChooser");
 
 			if (ViewModel.Property.CanWrite) {
-				// Handle changes on the view model
-				ViewModel.PropertyChanged += ViewModel_PropertyChanged;
-
 				// Handle color space changes
 				colorSpacePicker.SelectionChanged += (s, e) => {
 					if (ViewModel != null && ViewModel.Value != null) {
@@ -75,95 +58,8 @@ namespace Xamarin.PropertyEditing.Windows
 				blueEntry.LostFocus += (s, e) => UpdateFromTextBoxes ();
 				alphaEntry.PreviewTextInput += ConstrainToDigits;
 				alphaEntry.LostFocus += (s, e) => UpdateFromTextBoxes ();
-
-				// Handle interaction with the top shade chooser
-				topShadeChooser.MouseLeftButtonDown += OnShadeChanged;
-				topShadeChooser.MouseMove += (s, e) => {
-					if (e.LeftButton == MouseButtonState.Pressed) {
-						OnShadeChanged(s, e);
-					}
-				};
 			}
 		}
-
-		private void ViewModel_PropertyChanged (object sender, PropertyChangedEventArgs e)
-		{
-			// Handle interaction with the hue chooser
-			if (e.PropertyName == "Hue") {
-				var color = GetColorFromPosition (CurrentCursorPosition);
-				UpdateCurrentColor (color.R, color.G, color.B, skipHueUpdate: true);
-			}
-		}
-
-		protected override void OnRenderSizeChanged (SizeChangedInfo sizeInfo)
-		{
-			base.OnRenderSizeChanged (sizeInfo);
-			Update (ViewModel.Value.Color, skipHueUpdate: true);
-		}
-
-		void OnShadeChanged (object s, MouseEventArgs e)
-		{
-			CurrentCursorPosition = e.GetPosition ((IInputElement)s);
-			var color = GetColorFromPosition (CurrentCursorPosition);
-			UpdateCurrentColor (color.R, color.G, color.B, skipHueUpdate: true);
-		}
-
-		/// <summary>
-		/// Maps coordinates within the shade chooser gradients into colors.
-		/// The gradients have the current hue on the top-right corner,
-		/// black along the whole bottom border, and white of the top-left.
-		///
-		/// For example, with a hue of 128,255,0:
-		/// 
-		/// 255,255,255---192,255,128---128,255,000
-		///      |             |             |
-		/// 128,128,128---096,128,064---064,128,000
-		///      |             |             |
-		/// 000,000,000---000,000,000---000,000,000
-		/// 
-		/// The horizontal axis corresponds roughly to saturation, and the
-		/// vertical axis to brightness.
-		/// </summary>
-		/// <param name="position"></param>
-		/// <returns></returns>
-		CommonColor GetColorFromPosition (Point position)
-		{
-			var saturation = position.X / topShadeChooser.ActualWidth;
-			var brightness = 1 - position.Y / topShadeChooser.ActualHeight;
-
-			return new CommonColor (
-				(byte)((255 + (ViewModel.Hue.R - 255) * saturation) * brightness),
-				(byte)((255 + (ViewModel.Hue.G - 255) * saturation) * brightness),
-				(byte)((255 + (ViewModel.Hue.B - 255) * saturation) * brightness),
-				255
-			);
-		}
-
-		/// <summary>
-		/// Finds a position on the shade chooser that corresponds to the passed-in
-		/// color.
-		/// </summary>
-		/// <param name="color"></param>
-		/// <returns></returns>
-		Point GetPositionFromColor(CommonColor color, CommonColor hue)
-		{
-			var isRedMaxed = hue.R == 255;
-			var isGreenMaxed = hue.G == 255;
-			var alphaFactor = isRedMaxed ? (double)color.R / 255 : isGreenMaxed ? (double)color.G / 255 : (double)color.B / 255;
-			var isRedMin = hue.R == 0;
-			var isGreenMin = hue.G == 0;
-			var xrate =
-				isRedMin ? (color.R / alphaFactor - 255) / (hue.R - 255)
-				: isGreenMin ? (color.G / alphaFactor - 255) / (hue.G - 255)
-				: (color.B / alphaFactor - 255) / (hue.B - 255);
-			return new Point (
-				xrate * topShadeChooser.ActualWidth + topShadeChooser.Margin.Left - cursorWidth / 2,
-				(1 - alphaFactor) * topShadeChooser.ActualHeight + topShadeChooser.Margin.Top - cursorHeight / 2);
-		}
-
-		int[][] redRanges = new[] { new[] { 0, 1 }, new[] { 5, 6 } };
-		int[][] greenRanges = new[] { new[] { 1, 3 } };
-		int[][] blueRanges = new[] { new[] { 3, 5 } };
 
 		/// <summary>
 		/// Updates the view model, as well as the hue and shade controls
@@ -195,10 +91,10 @@ namespace Xamarin.PropertyEditing.Windows
 		/// <param name="green">The green component of the new color</param>
 		/// <param name="blue">The blue component of the new color</param>
 		/// <param name="skipHueUpdate">If true, skips updating the current hue and associated UI</param>
-		void UpdateCurrentColor (byte red, byte green, byte blue, bool skipHueUpdate = false, bool skipShadeUpdate = false)
+		void UpdateCurrentColor (byte red, byte green, byte blue, bool skipHueUpdate = false)
 		{
 			var alpha = ViewModel.Value.Color.A;
-			Update (new CommonColor (red, green, blue, alpha), skipHueUpdate, skipShadeUpdate);
+			Update (new CommonColor (red, green, blue, alpha), skipHueUpdate);
 		}
 
 		/// <summary>
@@ -206,7 +102,7 @@ namespace Xamarin.PropertyEditing.Windows
 		/// </summary>
 		/// <param name="newColor">The new color</param>
 		/// <param name="skipHueUpdate">If true, skips updating the current hue and associated UI</param>
-		void Update (CommonColor newColor, bool skipHueUpdate = false, bool skipShadeUpdate = false)
+		void Update (CommonColor newColor, bool skipHueUpdate = false)
 		{
 			ViewModel.Value = new CommonSolidBrush (newColor);
 
@@ -219,24 +115,6 @@ namespace Xamarin.PropertyEditing.Windows
 			// Update the shade picker's gradient to reflect the current hue
 			if (!skipHueUpdate) {
 				ViewModel.Hue = newColor.ToHue();
-			}
-			var newBrush = (LinearGradientBrush)bottomShadeChooser.Fill.Clone ();
-			var gradientStops = newBrush.GradientStops;
-			gradientStops.RemoveAt (1);
-			var currentHue = ViewModel.Hue;
-			gradientStops.Add (new GradientStop (Color.FromRgb (currentHue.R, currentHue.G, currentHue.B), 1));
-			bottomShadeChooser.Fill = newBrush;
-
-			if (!skipShadeUpdate) {
-				// Move the shade picker's cursor to the current color
-				CurrentCursorPosition = GetPositionFromColor (newColor, currentHue);
-				foreach (var child in cursor.Children) {
-					var shape = child as Shape;
-					if (shape != null) {
-						Canvas.SetTop (shape, CurrentCursorPosition.Y);
-						Canvas.SetLeft (shape, CurrentCursorPosition.X);
-					}
-				}
 			}
 		}
 
