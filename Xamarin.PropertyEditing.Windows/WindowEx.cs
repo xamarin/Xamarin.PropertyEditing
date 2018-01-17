9de8c5502fe007ccdc972ba62155d3333b8681c4
Eric Maupin <ermaup@microsoft.com>
2018-01-17 17:25:31 -0500

    [Win] Add minimize/maximize window toggle

--- Xamarin.PropertyEditing.Windows/WindowEx.cs
+++ Xamarin.PropertyEditing.Windows/WindowEx.cs
@@ -8,11 +8,25 @@ namespace Xamarin.PropertyEditing.Windows
 	internal class WindowEx
 		: Window
 	{
-		public static readonly DependencyProperty ShowIconProperty = DependencyProperty.Register ("ShowIcon", typeof(bool), typeof(WindowEx), new PropertyMetadata ((d, e) => ((WindowEx)d).UpdateExtendedStyles()));
+		public static readonly DependencyProperty ShowIconProperty = DependencyProperty.Register ("ShowIcon", typeof(bool), typeof(WindowEx), new PropertyMetadata (true, (d, e) => ((WindowEx)d).UpdateExtendedStyles()));
 		public bool ShowIcon
 		{
 			get { return (bool)GetValue (ShowIconProperty); }
 			set { SetValue (ShowIconProperty, value); }
+		}
+
+		public static readonly DependencyProperty ShowMaximizeProperty = DependencyProperty.Register ("ShowMaximize", typeof(bool), typeof(WindowEx), new PropertyMetadata (true, (d, e) => ((WindowEx)d).UpdateExtendedStyles()));
+		public bool ShowMaximize
+		{
+			get { return (bool)GetValue (ShowMaximizeProperty); }
+			set { SetValue (ShowMaximizeProperty, value); }
+		}
+
+		public static readonly DependencyProperty ShowMinimizeProperty = DependencyProperty.Register ("ShowMinimize", typeof(bool), typeof(WindowEx), new PropertyMetadata (true, (d, e) => ((WindowEx)d).UpdateExtendedStyles()));
+		public bool ShowMinimize
+		{
+			get { return (bool)GetValue (ShowMinimizeProperty); }
+			set { SetValue (ShowMinimizeProperty, value); }
 		}
 
 		protected override void OnSourceInitialized (EventArgs e)
@@ -31,26 +45,54 @@ namespace Xamarin.PropertyEditing.Windows
 			if (this.interop == null)
 				return;
 
-			int currentStyle = GetWindowLong (this.interop.Handle, WindowIndex.ExtendedStyle);
-			bool showingIcon = (currentStyle & WS_EX_DLGMODALFRAME) == 0;
-			if (ShowIcon == showingIcon)
-				return;
+			bool updateExStyle = false;
+			int currentExStyle = GetWindowLong (this.interop.Handle, WindowIndex.ExtendedStyle);
+			bool showingIcon = (currentExStyle & WS_EX_DLGMODALFRAME) == 0;
+			if (showingIcon != ShowIcon) {
+				if (ShowIcon && !showingIcon)
+					currentExStyle |= WS_EX_DLGMODALFRAME;
+				else
+					currentExStyle ^= WS_EX_DLGMODALFRAME;
 
-			if (ShowIcon && !showingIcon)
-				currentStyle |= WS_EX_DLGMODALFRAME;
-			else
-				currentStyle ^= WS_EX_DLGMODALFRAME;
+				SetWindowLong (this.interop.Handle, WindowIndex.ExtendedStyle, currentExStyle);
+				updateExStyle = true;
+			}
 
-			SetWindowLong (this.interop.Handle, WindowIndex.ExtendedStyle, currentStyle | WS_EX_DLGMODALFRAME);
-			SetWindowPos (this.interop.Handle, IntPtr.Zero, 0, 0, 0, 0, PositionFlags.NoMove | PositionFlags.NoSize | PositionFlags.NoZOrder | PositionFlags.FrameChanged);
+			int currentStyle = GetWindowLong (this.interop.Handle, WindowIndex.Style);
+			bool showingMinimize = (currentStyle & WS_MINIMIZEBOX) == WS_MINIMIZEBOX;
+			bool showingMaximize = (currentStyle & WS_MAXIMIZEBOX) == WS_MAXIMIZEBOX;
+
+			bool updateStyle = (showingMaximize != ShowMaximize || showingMinimize != ShowMinimize);
+			if (showingMaximize != ShowMaximize) {
+				if (ShowMaximize && !showingMaximize)
+					currentStyle |= WS_MAXIMIZEBOX;
+				else
+					currentStyle ^= WS_MAXIMIZEBOX;
+			}
+
+			if (showingMinimize != ShowMinimize) {
+				if (ShowMinimize && !showingMinimize)
+					currentStyle |= WS_MINIMIZEBOX;
+				else
+					currentStyle ^= WS_MINIMIZEBOX;
+			}
+
+			if (updateStyle)
+				SetWindowLong (this.interop.Handle, WindowIndex.Style, currentStyle);
+			
+			if (updateExStyle || updateStyle)
+				SetWindowPos (this.interop.Handle, IntPtr.Zero, 0, 0, 0, 0, PositionFlags.NoMove | PositionFlags.NoSize | PositionFlags.NoZOrder | PositionFlags.FrameChanged);
 		}
 
 		private enum WindowIndex
 		{
+			Style = -16,
 			ExtendedStyle = -20
 		}
 
 		const int WS_EX_DLGMODALFRAME = 0x001;
+		const int WS_MAXIMIZEBOX = 0x10000;
+		const int WS_MINIMIZEBOX = 0x20000;
 
 		[Flags]
 		private enum PositionFlags
