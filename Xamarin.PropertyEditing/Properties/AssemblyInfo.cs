3206d5e575798dd4b2c96bd183937f46e52f93ec
Bret Johnson <bret.johnson@microsoft.com>
2018-01-31 17:49:06 -0500

    Added (conditional) strong naming to builds
    Updated so if a ..\..\..\xamarin.snk file exists (which it will when we build proppy as git submodule under external/Xamarin.PropertyEditing),
    then we strong name the assemblies. Also updated InternalsVisibleTo to include public key when we build strong named, via ifdef.
    
    Note that there's still one outstanding issue here, with this build error:
    Xamarin.PropertyEditing\Xamarin.PropertyEditing.Windows\Themes\PropertyEditorPanelStyle.xaml(147,25): error MC3064: Only public or internal classes can be used within markup. 'EventViewModel' type is not public or internal.
    It seems like there's some bug with InternalsVisibleTo that includes a public key not working propertly from XAML files. But we can fix that (somehow) separately.

--- Xamarin.PropertyEditing/Properties/AssemblyInfo.cs
+++ Xamarin.PropertyEditing/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,6 +6,12 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible (false)]
 [assembly: Guid ("a0b6fe73-d046-4e1c-ba9d-f20683889c5a")]
 
+#if STRONG_NAMED
+[assembly: InternalsVisibleTo ("Xamarin.PropertyEditing.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010079159977d2d03a8e6bea7a2e74e8d1afcc93e8851974952bb480a12c9134474d04062447c37e0e68c080536fcf3c3fbe2ff9c979ce998475e506e8ce82dd5b0f350dc10e93bf2eeecf874b24770c5081dbea7447fddafa277b22de47d6ffea449674a4f9fccf84d15069089380284dbdd35f46cdff12a1bd78e4ef0065d016df")]
+[assembly: InternalsVisibleTo ("Xamarin.PropertyEditing.Windows, PublicKey=002400000480000094000000060200000024000052534131000400000100010079159977d2d03a8e6bea7a2e74e8d1afcc93e8851974952bb480a12c9134474d04062447c37e0e68c080536fcf3c3fbe2ff9c979ce998475e506e8ce82dd5b0f350dc10e93bf2eeecf874b24770c5081dbea7447fddafa277b22de47d6ffea449674a4f9fccf84d15069089380284dbdd35f46cdff12a1bd78e4ef0065d016df")]
+[assembly: InternalsVisibleTo ("Xamarin.PropertyEditing.Mac, PublicKey=002400000480000094000000060200000024000052534131000400000100010079159977d2d03a8e6bea7a2e74e8d1afcc93e8851974952bb480a12c9134474d04062447c37e0e68c080536fcf3c3fbe2ff9c979ce998475e506e8ce82dd5b0f350dc10e93bf2eeecf874b24770c5081dbea7447fddafa277b22de47d6ffea449674a4f9fccf84d15069089380284dbdd35f46cdff12a1bd78e4ef0065d016df")]
+#else
 [assembly: InternalsVisibleTo ("Xamarin.PropertyEditing.Tests")]
 [assembly: InternalsVisibleTo ("Xamarin.PropertyEditing.Windows")]
 [assembly: InternalsVisibleTo ("Xamarin.PropertyEditing.Mac")]
+#endif
