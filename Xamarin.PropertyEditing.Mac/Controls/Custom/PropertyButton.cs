baba14d76f481157545249c728d5d929a896e5b2
Dominique Louis <savagesoftware@gmail.com>
2018-07-12 17:05:01 +0100

    [Mac] Make PropertyButton NSImageView so it doesn't accept focus, but can still be interacted with.

--- Xamarin.PropertyEditing.Mac/Controls/Custom/PropertyButton.cs
+++ Xamarin.PropertyEditing.Mac/Controls/Custom/PropertyButton.cs
@@ -6,7 +6,7 @@ using Xamarin.PropertyEditing.ViewModels;
 
 namespace Xamarin.PropertyEditing.Mac
 {
-	public class PropertyButton : NSButton
+	public class PropertyButton : NSImageView
 	{
 		NSMenu popUpContextMenu;
 
@@ -33,19 +33,16 @@ namespace Xamarin.PropertyEditing.Mac
 		{
 			AccessibilityTitle = LocalizationResources.AccessibilityPropertiesButton;
 			AccessibilityHelp = LocalizationResources.AccessibilityPropertiesButtonDescription;
-			AlternateImage = NSImage.ImageNamed ("property-button-default-mac-active-10");
-			Cell = new NSButtonCell {
-				HighlightsBy = 1,
-			};
-			Bordered = false;
 			Enabled = true;
 			Image = NSImage.ImageNamed ("property-button-default-mac-10");
 			ImageScaling = NSImageScale.AxesIndependently;
-			Title = string.Empty;
 			ToolTip = Properties.Resources.Default;
 			TranslatesAutoresizingMaskIntoConstraints = false;
+		}
 
-			Activated += (sender, e) => {
+		public override void MouseDown (NSEvent theEvent)
+		{
+			if (theEvent.Type == NSEventType.LeftMouseDown) {
 				if (this.popUpContextMenu == null) {
 					this.popUpContextMenu = new NSMenu ();
 
@@ -54,7 +51,7 @@ namespace Xamarin.PropertyEditing.Mac
 							AttributedTitle = new Foundation.NSAttributedString (
 							Properties.Resources.CustomExpressionEllipsis,
 							new CoreText.CTStringAttributes () {
-								Font = new CoreText.CTFont (PropertyEditorControl.DefaultFontName, PropertyEditorControl.DefaultFontSize + 1),
+									Font = new CoreText.CTFont (PropertyEditorControl.DefaultFontName, PropertyEditorControl.DefaultFontSize + 1),
 							})
 						};
 
@@ -79,55 +76,102 @@ namespace Xamarin.PropertyEditing.Mac
 				var popupMenuEvent = NSEvent.MouseEvent (NSEventType.LeftMouseDown, menuOrigin, (NSEventModifierMask)0x100, 0, this.Window.WindowNumber, this.Window.GraphicsContext, 0, 1, 1);
 
 				NSMenu.PopUpContextMenu (popUpContextMenu, popupMenuEvent, this);
-			};
+			}
+		}
+
+		public override void MouseEntered (NSEvent theEvent)
+		{
+			ChangeActiveImage (viewModel.ValueSource, true);
+		}
+
+		public override void MouseExited (NSEvent theEvent)
+		{
+			ChangeActiveImage (viewModel.ValueSource);
+		}
+
+		public override void UpdateTrackingAreas ()
+		{
+			base.UpdateTrackingAreas ();
+
+			foreach (var item in TrackingAreas ()) {
+				RemoveTrackingArea (item);
+			}
+
+			var options = NSTrackingAreaOptions.MouseEnteredAndExited | NSTrackingAreaOptions.ActiveAlways;
+
+			var trackingArea = new NSTrackingArea (this.Bounds, options, this, null);
+
+			AddTrackingArea (trackingArea);
+		}
+
+		private void ChangeActiveImage (ValueSource valueSource, bool activeImage = false)
+		{
+			switch (valueSource) {
+				case ValueSource.Binding:
+					Image = activeImage ? NSImage.ImageNamed ("property-button-bound-mac-active-10") : NSImage.ImageNamed ("property-button-bound-mac-10");
+					break;
+
+				case ValueSource.Default:
+					Image = activeImage ? NSImage.ImageNamed ("property-button-default-mac-active-10") : NSImage.ImageNamed ("property-button-default-mac-10");
+					return;
+
+				case ValueSource.Local:
+					Image = activeImage ? NSImage.ImageNamed ("property-button-local-mac-active-10") : NSImage.ImageNamed ("property-button-local-mac-10");
+					break;
+
+				case ValueSource.Inherited:
+					Image = activeImage ? NSImage.ImageNamed ("property-button-inherited-mac-active-10") : NSImage.ImageNamed ("property-button-inherited-mac-10");
+					break;
+
+				case ValueSource.Resource:
+					Image = activeImage ? NSImage.ImageNamed ("property-button-inherited-mac-active-10") : NSImage.ImageNamed ("property-button-inherited-resource-mac-10");
+					break;
+
+				case ValueSource.Unset:
+					Image = activeImage ? NSImage.ImageNamed ("property-button-default-mac-active-10") : NSImage.ImageNamed ("property-button-default-mac-10");
+					break;
+
+				default:
+					// To Handle ValueSource.DefaultStyle, ValueSource.Style etc.
+					Image = null;
+					break;
+			}
 		}
 
 		private void ValueSourceChanged (ValueSource valueSource)
 		{
 			switch (valueSource) {
 				case ValueSource.Binding:
-					AlternateImage = NSImage.ImageNamed ("property-button-bound-mac-active-10");
-					Image = NSImage.ImageNamed ("property-button-bound-mac-10");
 					ToolTip = Properties.Resources.Binding;
 					break;
 
 				case ValueSource.Default:
-					AlternateImage = NSImage.ImageNamed ("property-button-default-mac-active-10");
-					Image = NSImage.ImageNamed ("property-button-default-mac-10");
 					ToolTip = Properties.Resources.Default;
 					return;
 
 				case ValueSource.Local:
-					AlternateImage = NSImage.ImageNamed ("property-button-local-mac-active-10");
-					Image = NSImage.ImageNamed ("property-button-local-mac-10");
 					ToolTip = Properties.Resources.Local;
 					break;
 
 				case ValueSource.Inherited:
-					AlternateImage = NSImage.ImageNamed ("property-button-inherited-mac-active-10");
-					Image = NSImage.ImageNamed ("property-button-inherited-mac-10");
 					ToolTip = Properties.Resources.Inherited;
 					break;
 
 				case ValueSource.Resource:
-					AlternateImage = NSImage.ImageNamed ("property-button-inherited-mac-active-10");
-					Image = NSImage.ImageNamed ("property-inherited-resource-mac-10");
 					ToolTip = (this.viewModel?.Resource?.Name != null) ? String.Format (Properties.Resources.ResourceWithName, this.viewModel.Resource.Name) : Properties.Resources.Resource;
 					break;
 
 				case ValueSource.Unset:
-					AlternateImage = NSImage.ImageNamed ("property-button-default-mac-active-10");
-					Image = NSImage.ImageNamed ("property-button-default-mac-10");
 					ToolTip = Properties.Resources.Unset;
 					break;
 
 				default:
 					// To Handle ValueSource.DefaultStyle, ValueSource.Style etc.
-					AlternateImage = null;
-					Image = null;
 					ToolTip = string.Empty;
 					break;
 			}
+
+			ChangeActiveImage (valueSource, false);
 		}
 
 		private void OnPropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
