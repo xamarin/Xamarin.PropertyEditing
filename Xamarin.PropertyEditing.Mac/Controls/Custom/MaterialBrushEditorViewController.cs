dc588486d12634da64e8fb15f11da7121989bf17
Larry Ewing <lewing@microsoft.com>
2018-07-15 13:36:48 -0500

    [mac] Add alpha editor on the material editor

--- Xamarin.PropertyEditing.Mac/Controls/Custom/MaterialBrushEditorViewController.cs
+++ Xamarin.PropertyEditing.Mac/Controls/Custom/MaterialBrushEditorViewController.cs
@@ -15,6 +15,8 @@ namespace Xamarin.PropertyEditing.Mac
 		}
 
 		private MaterialView materialEditor;
+		private AlphaChannelEditor alphaChannelEditor;
+		private ComponentSpinEditor alphaSpinEditor;
 
 		public override void OnPropertyChanged (object sender, PropertyChangedEventArgs e)
 		{
@@ -23,6 +25,8 @@ namespace Xamarin.PropertyEditing.Mac
 				case nameof (BrushPropertyViewModel.MaterialDesign):
 					if (this.materialEditor != null)
 						this.materialEditor.ViewModel = ViewModel;
+					if (this.alphaSpinEditor != null)
+						alphaSpinEditor.Value = alphaSpinEditor.ComponentEditor.ValueFromColor (ViewModel.Solid.Color);
 					break;
 			}
 		}
@@ -33,11 +37,58 @@ namespace Xamarin.PropertyEditing.Mac
 				this.materialEditor.ViewModel = ViewModel;
 		}
 
+		void UpdateComponent (object sender, EventArgs args)
+		{
+			if (ViewModel == null)
+				return;
+			
+			var color = ViewModel.Solid.Color;
+			var editor = sender as ComponentSpinEditor;
+			ViewModel.Solid.Color = editor.ComponentEditor.UpdateColorFromValue (color, editor.Value);
+			ViewModel.Solid.CommitLastColor ();
+		}
+
 		public override void LoadView ()
 		{
-			View = materialEditor = new MaterialView {
+			var stack = new NSStackView () {
+				Orientation = NSUserInterfaceLayoutOrientation.Vertical
+			};
+
+			this.materialEditor = new MaterialView {
 				ViewModel = ViewModel
 			};
+
+			this.alphaChannelEditor = new AlphaChannelEditor ();
+			this.alphaSpinEditor = new ComponentSpinEditor (this.alphaChannelEditor) {
+				BackgroundColor = NSColor.Clear,
+				TranslatesAutoresizingMaskIntoConstraints = true
+			};
+			this.alphaSpinEditor.ValueChanged += UpdateComponent;
+
+			var alphaStack = new NSStackView () {
+				Orientation = NSUserInterfaceLayoutOrientation.Horizontal
+			};
+
+			var alphaLabel = new NSTextField {
+				Bordered = false,
+				Editable = false,
+				Selectable = false,
+				ControlSize = NSControlSize.Small,
+				Font = NSFont.FromFontName (PropertyEditorControl.DefaultFontName, PropertyEditorControl.DefaultPropertyLabelFontSize),
+				AccessibilityElement = false,
+				StringValue = $"Alpha:",
+				Alignment = NSTextAlignment.Left,
+				BackgroundColor = NSColor.Clear,
+			};
+			alphaLabel.Cell.UsesSingleLineMode = true;
+			alphaLabel.Cell.LineBreakMode = NSLineBreakMode.Clipping;
+			alphaStack.AddView (alphaLabel, NSStackViewGravity.Trailing);
+			alphaStack.AddView (alphaSpinEditor, NSStackViewGravity.Trailing);
+
+			stack.AddView (this.materialEditor, NSStackViewGravity.Leading);
+			stack.AddView (alphaStack, NSStackViewGravity.Trailing);
+
+			View = stack;
 		}
 	}
 }
