3ba6aef64eb3f8933d47be598f32c44e7de5c22e
Larry Ewing <lewing@gmail.com>
2018-07-31 12:59:51 -0500

    Remove ChangingResourceProviderUpdatesResrouces test as it no longer applies

--- Xamarin.PropertyEditing.Tests/BrushPropertyViewModelTests.cs
+++ Xamarin.PropertyEditing.Tests/BrushPropertyViewModelTests.cs
@@ -52,26 +52,5 @@ namespace Xamarin.PropertyEditing.Tests
 			Assert.IsTrue (changed);
 			Assert.AreNotEqual (rs1, rs2);
 		}
-
-		[Test]
-		public void ChangingResourceProviderUpdatesResources ()
-		{
-			var mockProperty = new Mock<IPropertyInfo> ();
-			mockProperty.SetupGet (pi => pi.Type).Returns (typeof (CommonBrush));
-			var mockEditor = new MockObjectEditor (mockProperty.Object);
-			var vm = new BrushPropertyViewModel (new TargetPlatform (new MockEditorProvider (), new MockResourceProvider()), mockProperty.Object, new [] { mockEditor });
-			
-			var changed = false;
-			vm.PropertyChanged += (s, e) => {
-				if (e.PropertyName == nameof (BrushPropertyViewModel.ResourceSelector)) {
-					changed = true;
-				}
-			};
-			var rs1 = vm.ResourceSelector;
-			var rs2 = vm.ResourceSelector;
-			Assert.IsTrue (changed);
-			Assert.IsNull (rs1);
-			Assert.AreNotEqual (rs1, rs2);
-		}
 	}
 }
