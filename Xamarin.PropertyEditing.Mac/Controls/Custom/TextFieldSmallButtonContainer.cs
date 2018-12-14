0eb33d122736e2e80bc4aed791d75ebc332da71b
Jose Medrano <jose.medrano@microsoft.com>
2018-12-14 09:58:49 +0100

    [Mac] Adds a DirectoryPath Editor Fixes VSTS #732636 - Implement FilePath editor https://devdiv.visualstudio.com/DevDiv/_workitems/edit/732636

---
+++ Xamarin.PropertyEditing.Mac/Controls/Custom/TextFieldSmallButtonContainer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using AppKit;
+using CoreGraphics;
+using Foundation;
+
+namespace Xamarin.PropertyEditing.Mac
+{
+	class SmallButton : NSButton
+	{
+		NSView previousKeyView;
+		public override NSView PreviousKeyView => this.previousKeyView ?? base.PreviousKeyView;
+
+		internal void SetPreviousKeyView (NSView view)
+		{
+			this.previousKeyView = view;
+		}
+
+		public SmallButton ()
+		{
+			BezelStyle = NSBezelStyle.RegularSquare;
+			ControlSize = NSControlSize.Small;
+			Bordered = false;
+			TranslatesAutoresizingMaskIntoConstraints = false;
+			Cell.HighlightsBy = (int)NSCellStyleMask.ContentsCell;
+		}
+	}
+
+	class TextFieldSmallButtonContainer : NSTextField
+	{
+		public List<SmallButton> Buttons { get; } = new List<SmallButton> ();
+		readonly ButtonTextFieldCell cell;
+		internal NSView lastView;
+
+		public override bool Editable { 
+			get => base.Editable;
+			set
+			{
+				base.Editable = value;
+				foreach (var item in Buttons) {
+					item.Enabled = value;
+				}
+			}
+		}
+
+		public TextFieldSmallButtonContainer ()
+		{
+			Cell = this.cell = new ButtonTextFieldCell (this);
+			Cell.Editable = true;
+			Cell.Bezeled = true;
+			Cell.ControlSize = NSControlSize.Regular;
+			Cell.BezelStyle = NSTextFieldBezelStyle.Square;
+			TranslatesAutoresizingMaskIntoConstraints = false;
+
+			this.lastView = this;
+		}
+		public override bool AllowsVibrancy => false;
+
+		public override void DrawRect (CGRect dirtyRect)
+		{
+			// There appears to be a bug in AppKit in light theme when NSTextField is a subview
+			// of a layer-backed view, the background of the field does not get properly redrawn
+			// becase the dirtyRect is seemingly invalid. Fix this by redrawing the entire control.
+			// https://www.cocoanetics.com/2013/01/radar-nstextfield-redrawing-of-background-with-layer-backing/
+			// http://www.openradar.me/radar?id=2552401
+			base.DrawRect (Bounds);
+		}
+		public int ButtonSize { get; set; } = 16;
+
+		public int ButtonRightBorder { get; set; } = 3;
+
+		public int ButtonSeparation { get; set; } = 1;
+
+		public void AddButton (SmallButton button)
+		{
+			AddSubview (button);
+
+			var separation = Buttons.Count == 0 ? ButtonRightBorder : (ButtonSeparation + ButtonSize);
+
+			AddConstraints (new[] {
+				NSLayoutConstraint.Create (button, NSLayoutAttribute.CenterY, NSLayoutRelation.Equal, lastView, NSLayoutAttribute.CenterY, 1f, 0),
+				NSLayoutConstraint.Create (button, NSLayoutAttribute.Width, NSLayoutRelation.Equal, 1f, ButtonSize),
+				NSLayoutConstraint.Create (button, NSLayoutAttribute.Height, NSLayoutRelation.Equal, 1f, ButtonSize),
+				NSLayoutConstraint.Create (button, NSLayoutAttribute.Right, NSLayoutRelation.Equal, lastView, NSLayoutAttribute.Right, 1f, -separation),
+			});
+
+			Buttons.Add (button);
+			this.lastView = Buttons[Buttons.Count - 1];
+
+			//preview keyview calculation
+			button.SetPreviousKeyView (this);
+			if (Buttons.Count > 1) {
+				for (var i = Buttons.Count - 2; i >= 0; i--) {
+					Buttons[i].SetPreviousKeyView (Buttons[i+1]);
+				}
+			}
+		}
+
+		sealed class ButtonTextFieldCell : NSTextFieldCell
+		{
+			readonly TextFieldSmallButtonContainer field;
+
+			public ButtonTextFieldCell (TextFieldSmallButtonContainer field)
+			{
+				this.field = field;
+			}
+
+			public override void DrawInteriorWithFrame (CGRect cellFrame, NSView inView)
+			{
+				base.DrawInteriorWithFrame (DrawingRectForBounds (cellFrame), inView);
+			}
+
+			public override void EditWithFrame (CGRect aRect, NSView inView, NSText editor, NSObject delegateObject, NSEvent theEvent)
+			{
+				base.EditWithFrame (DrawingRectForBounds (aRect), inView, editor, delegateObject, theEvent);
+			}
+
+			public override void SelectWithFrame (CGRect aRect, NSView inView, NSText editor, NSObject delegateObject, nint selStart, nint selLength)
+			{
+				base.SelectWithFrame (DrawingRectForBounds (aRect), inView, editor, delegateObject, selStart, selLength);
+			}
+		
+			public override void ResetCursorRect (CGRect cellFrame, NSView inView)
+			{
+				base.ResetCursorRect (DrawingRectForBounds (cellFrame), inView);
+			}
+
+			public override CGRect DrawingRectForBounds (CGRect theRect)
+			{
+				var baseRect = base.DrawingRectForBounds (theRect);
+				if (this.field.Buttons.Count == 0) {
+					return baseRect;
+				}
+				return new CGRect (baseRect.X, baseRect.Y - 2, baseRect.Width - 30, 20);
+			}
+		}
+	}
+}
---
+++ Xamarin.PropertyEditing.Mac/Controls/FilePathEditorControl.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AppKit;
+using CoreGraphics;
+using Foundation;
+using Xamarin.PropertyEditing.Mac.Resources;
+using Xamarin.PropertyEditing.ViewModels;
+using System.IO;
+
+namespace Xamarin.PropertyEditing.Mac
+{
+	internal class FilePathEditorControl : PropertyEditorControl<PropertyViewModel<FilePath>>
+	{
+		readonly NSOpenPanel panel;
+		readonly TextFieldSmallButtonContainer currentTextField;
+		readonly SmallButton browsePathButton;
+		readonly SmallButton revealPathButton;
+
+		public override NSView FirstKeyView => this.currentTextField;
+		public override NSView LastKeyView => this.revealPathButton.Enabled ? this.revealPathButton : this.browsePathButton;
+
+		bool IsDirectory () => ViewModel.Value?.IsDirectory ?? false;
+		bool FileExists () => IsDirectory () ? Directory.Exists (this.currentTextField.StringValue) : File.Exists (this.currentTextField.StringValue);
+
+		public FilePathEditorControl (IHostResourceProvider hostResource)
+			: base (hostResource)
+		{
+			this.currentTextField = new TextFieldSmallButtonContainer ();
+			this.currentTextField.Changed += CurrentTextField_Changed;
+			AddSubview (this.currentTextField);
+
+			#region Reveal handler
+
+			this.revealPathButton = new SmallButton {
+				Font = NSFont.FromFontName (DefaultFontName, DefaultFontSize),
+				StringValue = string.Empty,
+				Image = hostResource.GetNamedImage ("path-reveal"),
+			};
+			this.currentTextField.AddButton (this.revealPathButton);
+
+			this.panel = new NSOpenPanel {
+				Title = string.Format (LocalizationResources.ChooseFileOrDirectory, LocalizationResources.File),
+				CanChooseFiles = true,
+				ShowsResizeIndicator = true,
+				ShowsHiddenFiles = false,
+				CanChooseDirectories = false,
+				CanCreateDirectories = true,
+				AllowsMultipleSelection = false
+			};
+
+			// update the value on keypress
+			this.revealPathButton.Activated += revealPathButton_Activated;
+
+			#endregion
+
+			#region Browse Path
+
+			this.browsePathButton = new SmallButton {
+				Font = NSFont.FromFontName (DefaultFontName, DefaultFontSize),
+				StringValue = string.Empty,
+				Image = hostResource.GetNamedImage ("path-browse"),
+			};
+			this.browsePathButton.Activated += BrowsePathButton_Activated;
+			this.currentTextField.AddButton (this.browsePathButton);
+
+			#endregion
+
+			AddConstraints (new[] {
+				NSLayoutConstraint.Create (this.currentTextField, NSLayoutAttribute.Height, NSLayoutRelation.Equal, 1, 20),
+				NSLayoutConstraint.Create (this.currentTextField, NSLayoutAttribute.CenterY, NSLayoutRelation.Equal, this, NSLayoutAttribute.CenterY, 1, 0),
+				NSLayoutConstraint.Create (this.currentTextField, NSLayoutAttribute.Width, NSLayoutRelation.Equal, this, NSLayoutAttribute.Width, 1, -34),
+			});
+		}
+
+		void CurrentTextField_Changed (object sender, EventArgs e)
+		{
+			StoreCurrentValue ();
+		}
+
+		void StoreCurrentValue ()
+		{
+			if (ViewModel.Value == null) {
+				return;
+			}
+			ViewModel.Value = new FilePath { Source = this.currentTextField.StringValue, IsDirectory = IsDirectory () };
+		}
+
+		void BrowsePathButton_Activated (object sender, EventArgs e)
+		{
+			this.panel.BeginSheet (this.Window, HandleAction);
+		}
+
+		void revealPathButton_Activated (object sender, EventArgs e)
+		{
+			if (FileExists ()) {
+				NSWorkspace.SharedWorkspace.OpenFile (this.currentTextField.StringValue);
+			}
+		}
+
+		void HandleAction (nint result)
+		{
+			if (result == 1) {
+				var url = this.panel.Url;
+				if (url.IsFileUrl) {
+					this.currentTextField.StringValue = url.Path;
+					StoreCurrentValue ();
+				}
+			}
+		}
+
+		protected override void UpdateValue ()
+		{
+			if (ViewModel.Value == null) {
+				this.revealPathButton.Alignment = NSTextAlignment.Center;
+				this.currentTextField.StringValue = "";
+			} else {
+				this.currentTextField.StringValue = ViewModel.Value.Source ?? string.Empty;
+				this.revealPathButton.Alignment = NSTextAlignment.Left;
+			}
+
+			if (IsDirectory ()) {
+				this.currentTextField.ToolTip = this.currentTextField.PlaceholderString = string.Format (LocalizationResources.ChooseFileOrDirectory, LocalizationResources.Directory);
+				this.revealPathButton.ToolTip = string.Format (LocalizationResources.RevealFileOrDirectory, LocalizationResources.Directory);
+				this.browsePathButton.ToolTip = string.Format (LocalizationResources.BrowseFileOrDirectory, LocalizationResources.Directory);
+
+				this.panel.CanChooseFiles = false;
+				this.panel.CanChooseDirectories = true;
+			} else {
+				this.currentTextField.ToolTip = this.currentTextField.PlaceholderString = string.Format (LocalizationResources.ChooseFileOrDirectory, LocalizationResources.File);
+				this.revealPathButton.ToolTip = string.Format (LocalizationResources.RevealFileOrDirectory, LocalizationResources.File);
+				this.browsePathButton.ToolTip = string.Format (LocalizationResources.BrowseFileOrDirectory, LocalizationResources.File);
+
+				this.panel.CanChooseFiles = true;
+				this.panel.CanChooseDirectories = false;
+			}
+			//button states
+			this.revealPathButton.Enabled = FileExists ();
+			Window?.RecalculateKeyViewLoop ();
+		}
+
+		protected override void HandleErrorsChanged (object sender, System.ComponentModel.DataErrorsChangedEventArgs e)
+		{
+			UpdateErrorsDisplayed (ViewModel.GetErrors (ViewModel.Property.Name));
+		}
+
+		protected override void UpdateErrorsDisplayed (IEnumerable errors)
+		{
+			if (ViewModel.HasErrors) {
+				SetErrors (errors);
+			} else {
+				SetErrors (null);
+				SetEnabled ();
+			}
+		}
+
+		protected override void SetEnabled ()
+		{
+			this.currentTextField.Enabled = ViewModel.Property.CanWrite;
+		}
+
+		protected override void UpdateAccessibilityValues ()
+		{
+			this.currentTextField.AccessibilityTitle = string.Format (LocalizationResources.AccessibilityString, ViewModel.Property.Name);
+		}
+
+		protected override void Dispose (bool disposing)
+		{
+			this.currentTextField.Changed -= CurrentTextField_Changed;
+			this.browsePathButton.Activated -= BrowsePathButton_Activated;
+			this.revealPathButton.Activated -= revealPathButton_Activated;
+			base.Dispose (disposing);
+		}
+	}
+}
