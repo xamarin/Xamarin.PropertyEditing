e5345bb6234617d8070f48376262226ea3af0e6e
Jose Medrano <jose.medrano@microsoft.com>
2019-01-18 16:39:11 +0100

    [Mac] Creates a new focusable ComboBox with readonly modifier
    Fixes #497

---
+++ Xamarin.PropertyEditing.Mac/Controls/Custom/FocusableComboBox.cs
@@ -0,0 +1,32 @@
+﻿using Foundation;
+using AppKit;
+
+namespace Xamarin.PropertyEditing.Mac
+{
+	class FocusableComboBox : NSComboBox
+	{
+		public override bool ShouldBeginEditing (NSText textObject)
+		{
+			textObject.Delegate = new FocusableComboBoxDelegate ();
+			return false;
+		}
+
+		public override void DidBeginEditing (NSNotification notification)
+		{
+			base.DidBeginEditing (notification);
+		}
+
+		class FocusableComboBoxDelegate : NSTextDelegate
+		{
+			public override bool TextShouldEndEditing (NSText textObject)
+			{
+				return false;
+			}
+
+			public override bool TextShouldBeginEditing (NSText textObject)
+			{
+				return false;
+			}
+		}
+	}
+}
