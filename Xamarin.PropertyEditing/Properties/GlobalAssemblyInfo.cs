2ca63696bc1f5e6515095082192c0833587d7ed7
Alan McGovern <alan@xamarin.com>
2019-03-21 11:20:59 +0000

    [build] Update to netstandard2 and/or net472

--- Xamarin.PropertyEditing/Properties/GlobalAssemblyInfo.cs
+++ Xamarin.PropertyEditing/Properties/GlobalAssemblyInfo.cs
@@ -1,12 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Microsoft Corporation")]
-[assembly: AssemblyProduct ("Xamarin.PropertyEditing")]
 [assembly: AssemblyCopyright ("Copyright © 2017 Microsoft Corporation")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
-
-[assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
