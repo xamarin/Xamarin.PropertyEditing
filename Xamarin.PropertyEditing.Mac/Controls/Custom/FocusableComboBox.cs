46748b5e70c99facdb52f4f5e01e21466f7889fa
Jose Medrano <josmed@microsoft.com>
2020-02-18 18:35:38 +0100

    Allows keydown in FocusableComboBox to filter values

--- Xamarin.PropertyEditing.Mac/Controls/Custom/FocusableComboBox.cs
+++ Xamarin.PropertyEditing.Mac/Controls/Custom/FocusableComboBox.cs
@@ -13,29 +13,5 @@ namespace Xamarin.PropertyEditing.Mac
 			}
 			return willBecomeFirstResponder;
 		}
-
-		public override bool ShouldBeginEditing (NSText textObject)
-		{
-			textObject.Delegate = new FocusableComboBoxDelegate ();
-			return false;
-		}
-
-		public override void DidBeginEditing (NSNotification notification)
-		{
-			base.DidBeginEditing (notification);
-		}
-
-		class FocusableComboBoxDelegate : NSTextDelegate
-		{
-			public override bool TextShouldEndEditing (NSText textObject)
-			{
-				return false;
-			}
-
-			public override bool TextShouldBeginEditing (NSText textObject)
-			{
-				return false;
-			}
-		}
 	}
 }
