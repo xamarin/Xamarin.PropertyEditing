45147ddbe52f859ed2f7d3f28172ac6eea33bc7b
Jose Medrano <josmed@microsoft.com>
2022-02-16 18:24:14 +0100

    Makes FocusablePopupButton a ProxyResponder class

--- Xamarin.PropertyEditing.Mac/Controls/Custom/FocusablePopupButton.cs
+++ Xamarin.PropertyEditing.Mac/Controls/Custom/FocusablePopupButton.cs
@@ -8,9 +8,27 @@ namespace Xamarin.PropertyEditing.Mac
 	{
 		public override bool CanBecomeKeyView { get { return Enabled; } }
 
+		public ProxyRowResponder ResponderProxy { get; set; }
+
 		public FocusablePopUpButton ()
 		{
 			Cell.LineBreakMode = NSLineBreakMode.TruncatingMiddle;
+		}
+
+		public override void KeyDown (NSEvent theEvent)
+		{
+			switch (theEvent.KeyCode) {
+			case (int)NSKey.Tab:
+				if (ResponderProxy != null) {
+					if (theEvent.ModifierFlags.HasFlag(NSEventModifierMask.ShiftKeyMask)) {
+						ResponderProxy.PreviousResponder ();
+					} else {
+						ResponderProxy.NextResponder ();
+					}
+				}
+				return;
+			}
+			base.KeyDown (theEvent);
 		}
 
 		public override bool BecomeFirstResponder ()
