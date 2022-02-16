0f4a14a4c2968b212a4109cfc1eb89867cab1bdc
Jose Medrano <josmed@microsoft.com>
2022-02-16 18:18:33 +0100

    Creates ProxyRowResponder manage responder between rows in NSOutlineView

---
+++ Xamarin.PropertyEditing.Mac/Controls/ProxyRowResponder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Xamarin.PropertyEditing.Mac
+{
+	internal enum ProxyRowType
+	{
+		SingleView,
+		FirstView,
+		LastView
+	}
+
+	internal class ProxyRowResponder
+	{
+		protected WeakReference<PropertyEditorControl> editorControl;
+
+		readonly ProxyRowType rowType;
+
+		public ProxyRowResponder (PropertyEditorControl editorControl, ProxyRowType rowType)
+		{
+			this.rowType = rowType;
+			this.editorControl = new WeakReference<PropertyEditorControl> (editorControl);
+		}
+
+		public bool NextResponder()
+		{
+			if (this.editorControl.TryGetTarget (out var editor))
+			{
+				if (this.rowType == ProxyRowType.LastView || this.rowType == ProxyRowType.SingleView) {
+					editor.OnNextResponderRequested ();
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public bool PreviousResponder ()
+		{
+			if (this.editorControl.TryGetTarget (out var editor))
+			{
+				if (this.rowType == ProxyRowType.FirstView || this.rowType == ProxyRowType.SingleView) {
+					editor.OnPreviousResponderRequested ();
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
