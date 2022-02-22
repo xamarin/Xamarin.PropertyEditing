c9da0507de991e74a7f39f9f78d1e351679a1964
Jose Medrano <josmed@microsoft.com>
2022-02-22 18:14:26 +0100

    Moves ProxyResponder button to a new class

--- Xamarin.PropertyEditing.Mac/Controls/Custom/FocusableBooleanButton.cs
+++ Xamarin.PropertyEditing.Mac/Controls/Custom/FocusableBooleanButton.cs
@@ -3,28 +3,6 @@ using AppKit;
 
 namespace Xamarin.PropertyEditing.Mac
 {
-	internal class ProxyResponderButton : NSButton
-	{
-		public RowProxyResponder ProxyResponder { get; set; }
-
-		public override void KeyDown (NSEvent theEvent)
-		{
-			switch (theEvent.KeyCode) {
-			case (int)NSKey.Tab:
-				if (ProxyResponder != null) {
-					if (theEvent.ModifierFlags.HasFlag(NSEventModifierMask.ShiftKeyMask)) {
-						ProxyResponder.PreviousResponder ();
-					} else {
-						ProxyResponder.NextResponder ();
-					}
-					return;
-				}
-				break;
-			}
-			base.KeyDown (theEvent);
-		}
-	}
-
 	internal class FocusableBooleanButton : ProxyResponderButton
 	{
 		public override bool CanBecomeKeyView { get { return Enabled; } }
